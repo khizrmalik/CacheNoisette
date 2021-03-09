e10fab711d9ca636a674a62897c4171bef288f0b
khizrmalik <khizm5050@gmail.com>
2021-03-09 09:33:22 +0400

    Create gameDriver.java

---
+++ gameDriver.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import javax.imageio.*;
+import java.io.*;
+
+public class gameDriver
+{
+    public static void main(String[] args)
+    {
+        CacheNoisette c = new CacheNoisette();
+    }
+}
